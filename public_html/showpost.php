10996d1b457ee4db813aaaee3a8627cce049f1cf
Eren Bilaloglu <eren.bilaloglu@ug.bilkent.edu.tr>
2017-12-21 08:25:11 +0000

    Add files via upload

--- public_html/showpost.php
+++ public_html/showpost.php
@@ -2,14 +2,17 @@
 <html>
 <?php
     include("session.php");
+	date_default_timezone_set('Europe/Istanbul');
+    $time = time();
+    $atime = date('Y-m-d H:i:s',$time);
     $postid = mysqli_real_escape_string($db,$_GET['id']);
-    
-    
+     
     if(isset($_POST["upvotepost"]))
     {
         $entryid = $_POST["upvotepostid"];
         $query = "SELECT * FROM Rates WHERE e_id = $entryid AND u_id = $login_id";
         $result = $db->query($query);
+
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
         if($count == 0)
@@ -119,7 +122,49 @@
         }
 
     }
+
+ 	if(isset($_POST["submitcomment"])){
+
+        $commenttext = mysqli_real_escape_string($db, $_POST['commenttext']);
+        $commentid = mysqli_real_escape_string($db, $_POST['submitcomment']);
+
+        $query = "INSERT INTO Entry (creationdate, content) VALUES ('$atime','$commenttext')";
+        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+
+        $query = "SELECT entryID FROM Entry WHERE creationdate = '$atime' AND content = '$commenttext'"; 
+        $result = $db->query($query);
+
+        while($row = $result->fetch_assoc()){
+            $entryID = $row["entryID"]; 
+        }
+
+        $query = "INSERT INTO Owns VALUES ($entryID, $login_id)";
+        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+
+        $query = "SELECT * FROM Postcomments, Subcomments WHERE c_id = $commentid || subcomment_id = $commentid"; 
+        $result = $db->query($query);
+
+        if ($result->num_rows > 0){
+
+	        $query = "INSERT INTO Comment VALUES ($entryID)";
+	        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+
+	        $query = "INSERT INTO Subcomments VALUES ($commentid, $entryID)";
+	        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+        } 
+
+        else{
+
+	        $query = "INSERT INTO Comment VALUES ($entryID)";
+	        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+
+	        $query = "INSERT INTO Postcomments VALUES ($postid, $entryID)";
+	        $data = mysqli_query ($db,$query)or die(mysqli_error($db));
+        }
+
+    }
     
+
 ?>
 <head>
     <meta charset="utf-8">
@@ -131,7 +176,7 @@
         function changeComment(entryid)
         {
             var submitComment = document.getElementsByName("submitcomment")[0];
-            submitComment.setAttribute("id", entryid);
+            submitComment.setAttribute("value", entryid);
             document.getElementsByName("submitcommentcontainer")[0].style.display = "block";
         }
     </script>
@@ -198,7 +243,7 @@
                 <ul class="list-group">
                     <li class="list-group-item list-group-item-info"><span>User: 
                     <?php
-                        $query = "SELECT * FROM Owns NATURAL JOIN User WHERE e_id = $postid";
+                        $query = "SELECT * FROM User u, Owns o WHERE o.e_id = '$postid' AND u.userID = o.u_id";
                         $result = $db->query($query);
                         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
                         $postownername = $row["username"];
@@ -226,7 +271,7 @@
                 <input value="<?php echo $postid;?>" type="hidden" name="downvotepostid">
                 <button class="btn btn-info active" type="submit" name="favoritepost">Add to Favorites</button>
                 <input value="<?php echo $postid;?>" type="hidden" name="favoritepostid">
-                
+                </form>
             </div>
         </div>
     </div>
@@ -235,19 +280,98 @@
             <div class="col-md-12">
                 <h4><em>Comments </em></h4></div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <blockquote>
-                    <p>End of Banking</p>
-                    <footer>george44 | Rating: 37</footer>
-                </blockquote>
-                <button class="btn btn-success active" type="button">Reply </button>
-                <button class="btn btn-primary active" type="button">Upvote </button>
-                <button class="btn btn-danger active" type="button">Downvote </button>
-                <button class="btn btn-info active" type="button">Add to Favorites</button>
-            </div>
-        </div>
-        <div class="row">
+        <?php
+
+       		function displaySub($commentID, $db, $depth, $offset){
+
+	            $query = "SELECT * FROM Subcomments WHERE comment_id = $commentID"; 
+	       		$result = $db->query($query);
+
+	       		if($result->num_rows == 0){
+	       			return;
+	       		}
+
+	       		else{
+	       				$depth = $depth-1;
+	       				$offset = $offset+1;
+
+		       			while($row = $result->fetch_assoc()){
+
+
+		       				$subno = $row["subcomment_id"];
+		       				$query2 = "SELECT * FROM Entry e, User u, Owns o WHERE e.entryID = $subno AND o.e_id = e.entryID AND u.userID = o.u_id";
+		       				$result2 = $db->query($query2);
+
+		       				while($row2 = $result2->fetch_assoc()){    
+
+		       					$subcontent = $row2["content"];
+		       					$subowner = $row2["username"];
+
+		       				}
+
+			            	echo '<div class="row">
+			            		<div class="col-md-'.$depth.' col-md-offset-'.$offset.' col-xs-'.$depth.' col-xs-offset-'.$offset.'">
+			              			<blockquote>
+				                    	<p>'.$subcontent.'</p>
+				                    	<footer>'.$subowner.' | Rating: 37</footer>
+					                </blockquote>
+					                <form method="post" action="">
+					                <button class="btn btn-success active" type="button" name="replypost" onmousedown="changeComment('.$subno.')">Reply </button>
+					                <input value="'.$subno.'" type="hidden" name="replypostid">
+					                <button class="btn btn-primary active" type="submit" name="upvotepost">Upvote </button>
+					                <input value="'.$subno.'" type="hidden" name="upvotepostid">
+					                <button class="btn btn-danger active" type="submit" name="downvotepost">Downvote </button>
+					                <input value="'.$subno.'" type="hidden" name="downvotepostid">
+					                <button class="btn btn-info active" type="submit" name="favoritepost">Add to Favorites</button>
+					                <input value="'.$subno.'" type="hidden" name="favoritepostid">
+					                </form>
+			            		</div>
+			        		</div>';
+
+			        		displaySub($subno,$db,$depth,$offset);
+	       				}	       			
+	       			}
+	       		}
+
+            $query = "SELECT p.c_id, e.* FROM Entry e, Postcomments p WHERE (p.p_id = '$postid' AND e.entryID = p.c_id) ORDER BY creationdate ASC"; 
+       		$result = $db->query($query);
+       		
+      		while($row = $result->fetch_assoc()){
+
+	            $query2 = "SELECT * FROM User u, Owns o WHERE o.e_id = '$row[entryID]' AND u.userID = o.u_id"; 
+	       		$result2 = $db->query($query2);  
+	       		while($row2 = $result2->fetch_assoc()){          	
+	       			$commentowner = $row2["username"];
+	       			$commentno = $row2["e_id"];
+	       		}
+
+	       		$depth = 12;
+	       		$offset = 1;
+            	echo '<div class="row">
+            		<div class="col-md-'.$depth.' col-md-offset-'.$offset.' col-xs-'.$depth.' col-xs-offset-'.$offset.'">
+              			<blockquote>
+	                    	<p>'.$row["content"].'</p>
+	                    	<footer>'.$commentowner.' | Rating: 37</footer>
+		                </blockquote>
+		                <form method="post" action="">
+		                <button class="btn btn-success active" type="button" name="replypost" onmousedown="changeComment('.$commentno.')">Reply </button>
+		                <input value="'.$commentno.'" type="hidden" name="replypostid">
+		                <button class="btn btn-primary active" type="submit" name="upvotepost">Upvote </button>
+		                <input value="'.$commentno.'" type="hidden" name="upvotepostid">
+		                <button class="btn btn-danger active" type="submit" name="downvotepost">Downvote </button>
+		                <input value="'.$commentno.'" type="hidden" name="downvotepostid">
+		                <button class="btn btn-info active" type="submit" name="favoritepost">Add to Favorites</button>
+		                <input value="'.$commentno.'" type="hidden" name="favoritepostid">
+		                </form>
+            		</div>
+        		</div>';
+
+        		displaySub($commentno,$db,$depth,$offset);
+			}
+		    
+        ?>
+
+        <!--<div class="row">
             <div class="col-md-11 col-md-offset-1 col-xs-11 col-xs-offset-1">
                 <blockquote>
                     <p>Do you really think that a virtual currency will bring the end of a solid economical system?</p>
@@ -282,18 +406,19 @@
                 <button class="btn btn-danger active" type="button">Downvote </button>
                 <button class="btn btn-info active" type="button">Add to Favorites</button>
             </div>
-        </div>
+        </div>-->
     </div>
+<form method="post" action="">
     <div class="container" type="hidden" style="display: none" name="submitcommentcontainer">
         <div class="row">
             <div class="col-md-12">
                 <h4>New Comment</h4>
-                <textarea class="input-lg">Put your comment here</textarea>
+                <textarea class="input-lg" name="commenttext">Put your comment here</textarea>
             </div>
         </div>
         <div class="row">
             <div class="col-md-12">
-                <button class="btn btn-success" type="submit" name="submitcomment" id="-1">Submit </button>
+                <button class="btn btn-success" type="submit" name="submitcomment" value="-1">Submit </button>
             </div>
         </div>
     </div>
